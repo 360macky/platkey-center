0fb1016b8e714fffae66796840b2745ac8f2b9db
Marcelo Arias <hello@marceloarias.com>
2023-02-06 14:46:15 -0500

    Create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { t } from 'i18next';
+
+const Footer = () => {
+  return (
+    <footer className="bg-black p-4">
+      <div className="flex flex-row justify-center gap-x-[2rem]">
+        <a
+          className="text-green hover:underline"
+          href="https://github.com/360macky/platkey"
+        >
+          {t('footer.githubrepository')}
+        </a>
+        <a
+          className="text-green hover:underline"
+          href="https://github.com/360macky/platkey/issues"
+        >
+          {t('footer.issues')}
+        </a>
+        <a
+          className="text-green hover:underline"
+          href="https://github.com/360macky/platkey#-contributing"
+        >
+          {t('footer.contributions')}
+        </a>
+      </div>
+      <div className="flex flex-row p-4 justify-center">
+        <p className="text-white text-center max-w-[42rem]">
+          {t('footer.disclaimer')}
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
