aedd331b7a00d1bf9e293d73312143e3943109cc
Marcelo Arias <hello@marceloarias.com>
2023-02-06 14:45:15 -0500

    Add basic PlatKey colors in TailwindCSS configuration file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        green: '#98CA3F',
+        blue: '#121F3D',
+        darkblue: '#0A1122',
+        skyblue: '#33B1FF',
+      },
+    },
+  },
+  plugins: [],
+};
