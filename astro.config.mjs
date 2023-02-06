6e34406192c45f122abb29c507b2aac26c13fb65
Marcelo Arias <hello@marceloarias.com>
2023-02-06 14:43:54 -0500

    Set up i18next in Astro configuration file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+
+// https://github.com/yassinedoghri/astro-i18next
+import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), astroI18next()],
 });
